782a072060ada95abd512c8e745b6df4f5b0a8a4
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-07-13 01:01:56 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,22 +3,32 @@
 // Создай галерею с возможностью клика по её элементам и просмотра полноразмерного изображения в модальном окне.
 // Выполняй это задание в файлах 01 - gallery.html и 01 - gallery.js.Разбей его на несколько подзадач:
 
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+// Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу.Запрети это поведение по умолчанию.
+    // evt.preventDefault(); //отмена автоматического перехода по ссылке
+  
+// 2.Реализация делегирования на div.gallery и получение url большого изображения.
+// 3.Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+
+//Подключаем в файле html
+//<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css" />
+
+//Скрипт подклбчаем перед!!! своим skript.js, чтобы мой .js уже видел библиотеку и выполнялся!!!
+//<script src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"></script>
+
+
+// 4.Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// 5.Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
 
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryBox = document.querySelector(".gallery");
 const galleryImagesMarkup = createGalleryItems(galleryItems);
-// console.log(createGalleryItems(galleryItems));
 
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 galleryBox.insertAdjacentHTML("beforeend", galleryImagesMarkup);
-
-galleryBox.addEventListener('click', onGalleryClick)
 
 function createGalleryItems(galleryItems) {
   return galleryItems
@@ -31,15 +41,20 @@ function createGalleryItems(galleryItems) {
   }).join('');
 }
 
+// 2.Реализация делегирования на div.gallery и получение url большого изображения.
+galleryBox.addEventListener('click', onGalleryClick);
+
 function onGalleryClick(evt) {
-
   evt.preventDefault(); //отмена автоматического перехода по ссылке
-
   const isGalleryItem = evt.target.classList.contains('gallery__image');
-
   if (!isGalleryItem) {
     return;
   }
-    console.log(evt.target.dataset.source);
-
+ 
+  // 4.Открытие модального окна по клику на элементе галереи. Замена значения атрибута src элемента <img> в модальном окне перед открытием
+  const targetItemAddr = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${targetItemAddr}">
+`);
+  instance.show();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,60 @@
+// Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. Разбей его на несколько подзадач:
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. 
+//Используй готовый код из первого задания.
+// 2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+// 3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// 4. Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector(".gallery");
+const galleryImagesMarkup = createGalleryItems(galleryItems);
+
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+galleryBox.insertAdjacentHTML("beforeend", galleryImagesMarkup);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+         </li> `;
+  }).join('');
+}
+
+galleryBox.addEventListener('click', onGalleryClick);
+
+import SimpleLightbox from "simplelightbox";
+
+// function onGalleryClick(evt) {
+//   evt.preventDefault(); //отмена автоматического перехода по ссылке
+//   const isGalleryItem = evt.target.classList.contains('gallery__image');
+//   if (!isGalleryItem) {
+//     return;
+//   }
+    
+    let gallery = new SimpleLightbox('.gallery__item');
+gallery.on('show.simplelightbox', function () {
+	// Do something…
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // Some usefull information
+});
+
+// with jQuery nearly the same
+let gallery = $('.gallery a').simpleLightbox();
+gallery.on('show.simplelightbox', function () {
+	// Do something…
+});
+
+//     <div class="gallery">
+//     <a href="${original}"><img src="${preview}" alt="${description}" title="${description}"/></a>
+//     <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
+// </div>
